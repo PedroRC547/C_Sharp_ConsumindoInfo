84f176cfb6f6b8a3f65f31a4516ed493435181ed
PedroRC547 <candianipedro547@gmail.com>
2023-02-28 00:50:26 -0300

    C_Sharp consumindo API

---
+++ Entities/Aluno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumindoApi.Entities
+{
+    internal class Aluno
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Situacao { get; set; }
+
+        public int Matricula { get; set; }
+
+        public bool Verificacao { get; set; }
+
+    }
+}
